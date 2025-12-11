5598b23de2318c0ed25cfdd3468d8dc19032bebb
Eric Rochester <erochest@gmail.com>
2025-12-11 07:09:56 -0500

    use thiserror

--- src/error.rs
+++ src/error.rs
@@ -4,28 +4,10 @@ use std::fmt;
 use std::io;
 use std::result;
 
-pub type Result<R> = result::Result<R, Error>;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum Error {
-    IoError(io::Error),
-}
-
-use Error::*;
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            IoError(ref err) => err.fmt(f),
-        }
-    }
-}
-
-impl error::Error for Error {
-}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error {
-        IoError(err)
-    }
+    #[error("IO error: {0}")]
+    IoError(#[from] io::Error),
 }
